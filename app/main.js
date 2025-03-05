59ab15df60df46b8bf98ef6ccee83e552c06bf0a
Olsi Gjeci <olsi.developer@gmail.com>
2025-03-05 23:18:23 +0000

    echo

--- app/main.js
+++ app/main.js
@@ -10,7 +10,39 @@ function prompt() {
     if (answer === "exit 0") {
       process.exit(0);
     } else {
-      console.log(`${answer}: command not found`);
+      const [command, ...args] = answer.split(" ");
+      if (command === "echo") {
+        console.log(args.join(" "));
+      } 
+      // else if (command === "cat") {
+      //   const fs = require("fs");
+      //   const file = args[0];
+      //   fs.readFile(file, "utf8", (err, data) => {
+      //     if (err) {
+      //       console.log(err);
+      //     } else {
+      //       console.log(data);
+      //     }
+      //     prompt();
+      //   });
+      //   return;
+      // } else if (command === "ls") {
+      //   const fs = require("fs");
+      //   fs.readdir(".", (err, files) => {
+      //     if (err) {
+      //       console.log(err);
+      //     } else {
+      //       console.log(files.join(" "));
+      //     }
+      //     prompt();
+      //   });
+      //   return;
+      // } else if (command === "pwd") {
+      //   console.log(process.cwd());
+      // } else {
+      //   console.log(`${command}: command not found`);
+      // }
+      // console.log(`${answer}: command not found`);
       prompt();
     }
   });    
