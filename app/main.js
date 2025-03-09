1514a45c040d25141e80c0a84b61b624a0da2600
Olsi Gjeci <olsi.developer@gmail.com>
2025-03-09 23:23:11 +0000

    builtin command type

--- app/main.js
+++ app/main.js
@@ -1,52 +1,55 @@
-const readline = require("readline");
+const readline = require('readline');
+const { exec } = require('child_process');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+function isBuiltin(command, callback) {
+  exec(`type ${command}`, (error, stdout, stderr) => {
+    if (error) {
+      // Command not found or other error
+      callback(false);
+      return;
+    }
+
+    if (stdout.includes('builtin')) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
 function prompt() {
-  rl.question("$ ", (answer) => {
-    if (answer === "exit 0") {
+  rl.question('$ ', answer => {
+    if (answer === 'exit 0') {
       process.exit(0);
     } else {
-      const [command, ...args] = answer.split(" ");
-      if (command === "echo") {
-        console.log(args.join(" "));
+      const [command, ...args] = answer.split(' ');
+
+      console.log('🚀 ~ prompt ~ command:', command);
+      console.log('🚀 ~ prompt ~ args:', args);
+
+      if (command === 'echo') {
+        console.log(args.join(' '));
+      } else if (command === 'type') {
+        const [subCommand] = args;
+        isBuiltin(subCommand, isBuiltIn => {
+          console.log(
+            isBuiltIn
+              ? `${subCommand} is a shell builtin`
+              : `${subCommand} not found`,
+          );
+        });
       } else {
         console.log(`${answer}: command not found`);
       }
-      // else if (command === "cat") {
-      //   const fs = require("fs");
-      //   const file = args[0];
-      //   fs.readFile(file, "utf8", (err, data) => {
-      //     if (err) {
-      //       console.log(err);
-      //     } else {
-      //       console.log(data);
-      //     }
-      //     prompt();
-      //   });
-      //   return;
-      // } else if (command === "ls") {
-      //   const fs = require("fs");
-      //   fs.readdir(".", (err, files) => {
-      //     if (err) {
-      //       console.log(err);
-      //     } else {
-      //       console.log(files.join(" "));
-      //     }
-      //     prompt();
-      //   });
-      //   return;
-      // } else if (command === "pwd") {
-      //   console.log(process.cwd());
-      // } else {
-      //   console.log(`${command}: command not found`);
-      // }
+
       prompt();
     }
-  });    
+  });
 }
 
 prompt();
