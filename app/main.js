d112fc76684bd6e9c9e945ecfad6d35b16c04623
Olsi Gjeci <olsi.developer@gmail.com>
2025-03-15 18:35:25 +0000

    run a program

--- app/main.js
+++ app/main.js
@@ -1,92 +1,5 @@
-// const readline = require('readline');
-// const { exec } = require('child_process');
-// const path = require('path');
-// const fs = require('fs');
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// function isBuiltin(command, callback) {
-//   exec(`type ${command}`, (error, stdout) => {
-//     if (error) {
-//       callback(false);
-//       return;
-//     }
-
-//     if (stdout.includes('builtin')) {
-//       callback(true);
-//     } else {
-//       callback(false);
-//     }
-//   });
-// }
-
-// function prompt() {
-//   rl.question('$ ', answer => {
-//     if (answer === 'exit 0') {
-//       process.exit(0);
-//     } else {
-//       const [command, ...args] = answer.split(' ');
-
-//       if (command === 'echo') {
-//         console.log(args.join(' '));
-//         prompt();
-//         return;
-//       }
-
-//       if (command === 'type') {
-//         const [subCommand] = args;
-
-//         if (['echo', 'type', 'exit'].includes(subCommand)) {
-//           console.log(`${subCommand} is a shell builtin`);
-//           prompt();
-//           return;
-//         }
-
-//         if (subCommand === 'invalid_command') {
-//           console.log(`${invalid_command} not found`);
-//           prompt();
-//           return;
-//         }
-
-//         const paths = process.env.PATH.split(path.delimiter);
-//         for (const p of paths) {
-//           const fullPath = path.join(p, subCommand);
-
-//           if (fs.existsSync(fullPath)) {
-//             console.log(`${subCommand} is ${fullPath}`);
-//             prompt();
-//             return;
-//           }
-//         }
-
-//         console.log(`${subCommand}: not found`);
-
-//         // isBuiltin(subCommand, builtin => {
-//         //   if (builtin) {
-//         //     console.log(`${subCommand} is a shell builtin`);
-//         //   } else {
-//         //     console.log(`${subCommand} not found`);
-//         //   }
-//         //   prompt();
-//         // });
-
-//         prompt();
-//         return;
-//       }
-
-//       console.log(`${answer}: command not found`);
-//       prompt();
-//     }
-//   });
-// }
-
-// prompt();
-
 const readline = require('readline');
-const { exec, execFileSync } = require('child_process');
+const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
@@ -94,17 +7,6 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-// Function to check if a command is a shell builtin
-// function isBuiltin(command, callback) {
-//   exec(`type ${command}`, (error, stdout) => {
-//     if (error) {
-//       callback(false);
-//       return;
-//     }
-//     callback(stdout.includes('builtin'));
-//   });
-// }
 
 // Function to handle the 'echo' command
 function handleEcho(args) {
@@ -150,19 +52,18 @@ function prompt() {
         handleType(args);
         break;
       default:
-        execFileSync(command, [args[0]], { encoding: 'utf8' });
-        console.log('Program was passed 2 args (including program name).');
-        // exec(fullCommand, (error, stdout) => {
-        //   if (error) {
-        //     console.log(`${command}: command not found`);
-        //     return;
-        //   }
-        //   if (stdout.includes('builtin')) {
-        //     console.log(`${command} is a shell builtin`);
-        //   } else {
-        //   }
-        // });
-        break;
+        exec(`${command} ${args.join(' ')}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`${command}: command not found`);
+          } else {
+            console.log(stdout);
+            if (stderr) {
+              console.error(stderr);
+            }
+          }
+          prompt();
+        });
+        return;
     }
 
     prompt();
